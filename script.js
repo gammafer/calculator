e3afb787ab3ee9b409d319bbded8ce823e75685f
gammafer <60357762+gammafer@users.noreply.github.com>
2021-07-03 17:25:05 +0200

    More styling, basic operations are working

--- script.js
+++ script.js
@@ -7,16 +7,71 @@ const multiply=(a,b)=>a*b;
 const divide=(a,b)=>a/b;
 
 let num=0;
+let acc=null;
+let operation;
 
-function getPushedButton(pushedButton){
-    console.log(pushedButton);
-    //writeToScreen();
-    if(pushedButton.target.classList[1]=='num'){
-        console.log("wow, it works!")
-    }
-
+function writeOnScreen(){
+    screen.innerHTML=num;
 }
 
+function pushedNumber(value){
+    if(num==0){
+        num=parseInt(value);
+        writeOnScreen();
+        return;
+    }
+    let newNumber=num.toString()+value.toString();
+    num=parseInt(newNumber);
+    writeOnScreen();  
+}
+
+
+function pushedOperation(value){
+    if(value=="=" && acc!=null){
+        switch(operation){
+            case "+":
+                num=add(acc,num);
+                break;
+            case "-":
+                num=substract(acc,num);
+                break;
+            case "x":
+                num=multiply(acc,num);
+                break;
+            case "÷":
+                num=divide(acc,num);
+                break;
+        }
+    writeOnScreen(); 
+    return; 
+    }
+    operation=value;
+        acc=num;
+        num=0;
+}
+
+
+
+function calculate(pushedButton){
+
+    let value=pushedButton.target.innerHTML;
+
+
+    if(pushedButton.target.classList[1]=='num'){
+           pushedNumber(value);
+    }
+
+    else if(pushedButton.target.classList[1]=='op'){
+           pushedOperation(value);   
+    }
+
+    console.log(acc,num);
+}
+
+
+
+writeOnScreen(); //A 0-t is írjuk ki egyből.
+
 Array.from(buttons).forEach(button => {
-    button.addEventListener('click',e=>getPushedButton(e));
+    button.addEventListener('click',e=>calculate(e));
 });
